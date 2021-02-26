b87e2b8f910d2a8054505029f8bd998d1343ab48
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2021-02-26 08:45:59 +0100

    T01: build function that opens up the modal

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-let oddNumber = 1;
-const ul = document.querySelector("ul");
+const cardButtons = document.querySelectorAll(".card");
+const modalOuter = document.querySelector(".modal-outer");
+const modalInner = document.querySelector(".modal-inner");
 
-const addLi = function () {
-  let li;
-  if (oddNumber % 3 == 0) {
-    li = `<li class="big">${oddNumber}</li>`;
-  } else {
-    li = `<li>${oddNumber}</li>`;
-  }
+/* Open Modal */
+const handleButtonClick = function (event) {
+  const button = event.currentTarget;
+  const card = button.closest(".card");
+  const imgSrc = card.querySelector("img").src;
+  const description = card.dataset.description;
+  const name = card.querySelector("h2").textContent;
+
+  modalInner.innerHTML = `<img src="${imgSrc}" alt="${name}"/>
+  <h2>${name}</h2>
+  <p>${description}</p>`;
+
+  modalOuter.classList.add("open");
+};
+
+cardButtons.forEach((button) =>
+  button.addEventListener("click", handleButtonClick)
+);
 
   const docFragmLi = document.createRange().createContextualFragment(li)
     .firstElementChild;
