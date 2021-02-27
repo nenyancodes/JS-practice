69ca9dc0153f9b9a0bf766cf2b0303069074ef6e
Ania Wójtowicz <wojtowicz.anna.maria+nenyan@gmail.com>
2021-02-27 23:48:13 +0100

    T03 TABS: Built showTab function

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+////// TASK 03: TABS
+
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll("[role='tab']");
+const tabPanels = Array.from(tabs.querySelectorAll("[role='tabpanel']"));
+
+function showTab(event) {
+  // hide all tab panels
+  tabPanels.forEach((panel) => (panel.hidden = true));
+  // mark all tabs as unselected
+  tabButtons.forEach((button) => {
+    // button.ariaSelected = false; - won't work on aria attributes and custom attrs
+    button.setAttribute("aria-selected", false);
+  });
+  // mark clicked tab as selected
+  event.currentTarget.setAttribute("aria-selected", true);
+  // show clicked tab panel
+  const { id } = event.currentTarget;
+
+  // first solution
+  // const clickedTabPanel = tabs.querySelector(`[aria-labelledby="${id}"`);
+  // clickedTabPanel.hidden = false;
+
+  // second solution - remember that find() works only on arrays not on NodeLists!
+  const clickedTabPanel = tabPanels.find(
+    (panel) => panel.getAttribute("aria-labelledby") == id
+  );
+  clickedTabPanel.hidden = false;
+}
+tabButtons.forEach((button) => button.addEventListener("click", showTab));
+
 ///// TASK 02: INTERSECTION OBSERVER
 
 const terms = document.querySelector(".terms");
